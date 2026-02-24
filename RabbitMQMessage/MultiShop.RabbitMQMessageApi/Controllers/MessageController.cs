6da13c283af8e8650f7e1270831ee86cef92582e
Mahmut Baglama <mahmutbaglama98@gmail.com>
2026-02-25 02:41:38 +0300

    Mikroservis

---
+++ RabbitMQMessage/MultiShop.RabbitMQMessageApi/Controllers/MessageController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using RabbitMQ.Client;
+using System.Text;
+
+namespace MultiShop.RabbitMQMessageApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MessageController : ControllerBase
+    {
+        [HttpPost]
+        public async Task<IActionResult> CreateMessage()
+        {
+            var connectionfactory = new ConnectionFactory()
+            {
+                HostName= "localhost",
+                
+            };
+            var connection= connectionfactory.CreateConnection();
+            var channel = connection.CreateModel();
+            channel.QueueDeclare("Kuyruk1",false,false,false,arguments:null);
+            var messageContent="Merhaba bu bir RabbitMQ kuyruk mesajıdır";
+            var byteMessageContent=Encoding.UTF8.GetBytes(messageContent);
+            channel.BasicPublish(exchange: "", routingKey: "Kuyruk1", basicProperties: null,body:byteMessageContent);
+
+            return Ok("Mesajınız alınmıştır");
+        }
+       
+    }
+}
