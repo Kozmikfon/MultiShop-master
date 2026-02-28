5bdd9bf4aa639834931cd486b1f19690409bfedf
Mahmut Baglama <mahmutbaglama98@gmail.com>
2026-03-01 02:40:35 +0300

    MultShop.Seller.BusinessLayer katmanı yapısı tanımlandı.

---
+++ Services/Seller/MultiShop.Seller.Businesslayer/Abstract/IVendorService.cs
@@ -0,0 +1,13 @@
+﻿using MultiShop.Seller.EntityLayer.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultiShop.Seller.Businesslayer.Abstract
+{
+    public interface IVendorService :IGenericService<Vendor>
+    {
+    }
+}
---
+++ Services/Seller/MultiShop.Seller.Businesslayer/Concrete/VendorManager.cs
@@ -0,0 +1,44 @@
+﻿using MultiShop.Seller.Businesslayer.Abstract;
+using MultiShop.Seller.DataAccessLayer.Abstract;
+using MultiShop.Seller.EntityLayer.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultiShop.Seller.Businesslayer.Concrete
+{
+    public class VendorManager : IVendorService
+    {
+        private readonly IVendorDal _vendorDal;
+        public VendorManager(IVendorDal vendorDal)
+        {
+            _vendorDal = vendorDal; 
+        }
+        public async Task TDeleteAsync(int id)
+        {
+           await _vendorDal.DeleteAsync(id);
+        }
+
+        public async Task<List<Vendor>> TGetAllAsync()
+        {
+           return await _vendorDal.GetAllAsync();
+        }
+
+        public async Task<Vendor> TGetByIdAsync(int id)
+        {
+            return await _vendorDal.GetByIdAsync(id);
+        }
+
+        public async Task TInsertAsync(Vendor entity)
+        {
+            await _vendorDal.InsertAsync(entity);
+        }
+
+        public async Task TUpdateAsync(Vendor entity)
+        {
+            await _vendorDal.UpdateAsync(entity);
+        }
+    }
+}
