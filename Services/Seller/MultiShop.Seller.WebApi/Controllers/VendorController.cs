34bd15746c660750c48387d0444768a9c634607b
Mahmut Baglama <mahmutbaglama98@gmail.com>
2026-03-01 16:30:33 +0300

    MultiShop.Seller.WebAp controller işlemi tamamlandı.

--- Services/Seller/MultiShop.Seller.WebApi/Controllers/VendorController.cs
+++ Services/Seller/MultiShop.Seller.WebApi/Controllers/VendorController.cs
@@ -12,7 +12,43 @@ namespace MultiShop.Seller.WebApi.Controllers
     [ApiController]
     public class VendorController : ControllerBase
     {
-       
-            
+        private readonly IVendorService _vendorService;
+
+        public VendorController(IVendorService vendorService)
+        {
+            _vendorService = vendorService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> VendorList()
+        {
+            var values = await _vendorService.TGetAllAsync();
+            return Ok(values);
+        }
+        [HttpPost]
+        public async Task<IActionResult> CreateVendor(CreateVendorDto createVendorDto)
+        {
+            await _vendorService.TInsertAsync(createVendorDto);
+            return Ok("Vendor Başarıyla Oluşturuldu");
+        }
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetByIdVendor(int id)
+        {
+            var value = await _vendorService.TGetByIdAsync(id);
+            return Ok(value);
+        }
+        [HttpDelete]
+        public async Task<IActionResult> RemoveVendor(int id)
+        {
+            await _vendorService.TDeleteAsync(id);
+            return Ok("Vendor Başarıyla Silindi");
+        }
+        [HttpPut]
+        public async Task<IActionResult> UpdateVendor(UpdateVendorDto updateVendorDto)
+        {
+            await _vendorService.TUpdateAsync(updateVendorDto);
+            return Ok("Vendor Başarıyla Güncellendi");
+
+        }
     }
 }
