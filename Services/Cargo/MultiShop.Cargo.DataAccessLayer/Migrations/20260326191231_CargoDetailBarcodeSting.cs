cfd2cacaf4725bcc0f08bca7a5b175fdd4d449fb
Mahmut Baglama <mahmutbaglama98@gmail.com>
2026-03-26 22:14:56 +0300

    Cargo ve order işlemlerinde masstrnasit kütüphanesi kuruldu Cargo.Web.Apide OrderPaidConsumer sınıfını yazdık, cargoDetail entity'sinde barcode alanı int türünden string türüne çevrildi.

---
+++ Services/Cargo/MultiShop.Cargo.DataAccessLayer/Migrations/20260326191231_CargoDetailBarcodeSting.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MultiShop.Cargo.DataAccessLayer.Migrations
+{
+    public partial class CargoDetailBarcodeSting : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Barcode",
+                table: "CargoDetails",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "Barcode",
+                table: "CargoDetails",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+        }
+    }
+}
---
+++ Services/Cargo/MultiShop.Cargo.WebApi/Consumers/OrderPaidConsumer.cs
@@ -0,0 +1,59 @@
+﻿using MassTransit;
+using MultiShop.Cargo.BusinessLayer.Abstract;
+using MultiShop.Cargo.DataAccessLayer.Abstract;
+using MultiShop.Cargo.EntityLayer.Concrete;
+using MultiShop.Cargo.EntityLayer.Concrete.Enums;
+using MultiShop.Shared.Events;
+
+namespace MultiShop.Cargo.WebApi.Consumers
+{
+    public class OrderPaidConsumer : IConsumer<IOrderPaidEvent>
+    {
+        private readonly IShipinkService _shipinkService;
+        private readonly ICargoDetailDal _cargoDetaildal;
+
+        public OrderPaidConsumer(IShipinkService shipinkService, ICargoDetailDal cargoDetaildal)
+        {
+            _shipinkService = shipinkService;
+            _cargoDetaildal = cargoDetaildal;
+        }
+
+        // DİKKAT: Buraya 'async' ekledik!
+        public async Task Consume(ConsumeContext<IOrderPaidEvent> context)
+        {
+            var cargoDetail = new CargoDetail
+            {
+                OrderingId = context.Message.OrderingId,
+                ReceiverName = context.Message.ReceiverName,
+                ReceiverSurname = context.Message.ReceiverSurname,
+                ReceiverEmail = context.Message.ReceiverEmail,
+                ReceiverPhone = context.Message.ReceiverPhone,
+                ReceiverCity = context.Message.ReceiverCity,
+                ReceiverDistrict = context.Message.ReceiverDistrict,
+                ReceiverAddressDetail = context.Message.ReceiverAddressDetail,
+                CargoCompanyId = context.Message.CargoCompanyId,
+                CurrentStatus = CargoStatus.Created,
+                Weight = 1.0,
+                Width = 10,
+                Height = 10,
+                Length = 10,
+                // Değişken ismini düzelttik: _cargoDetaildal
+                Barcode = "BRK-" + context.Message.OrderingId + "-" + DateTime.Now.Ticks.ToString().Substring(10)
+            };
+
+            // await kullandık
+            await _cargoDetaildal.Insert(cargoDetail);
+
+            try
+            {
+                // Shipink tetikleme
+                await _shipinkService.CreateShipmentAsync(cargoDetail.CargoDetailId);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Shipink Hatası: {ex.Message}");
+                throw;
+            }
+        }
+    }
+}
