8e0fc14b19da85c9be075608bea8d1eb4366eb47
Mahmut Baglama <mahmutbaglama98@gmail.com>
2026-03-26 12:07:08 +0300

    RabbitMq için bir ortak dil class library oluşturuldu, cargo ve payment referans olarak aldı.

---
+++ Services/MultiShop.Shared/Class1.cs
@@ -0,0 +1,7 @@
+﻿namespace MultiShop.Shared
+{
+    public class Class1
+    {
+
+    }
+}
---
+++ Services/Shared/MultiShop.Shared/Events/IOrderPaidEvent.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultiShop.Shared.Events
+{
+    public interface IOrderPaidEvent
+    {
+        int OrderingId { get; }
+        string ReceiverName { get; }
+        string ReceiverSurname { get; }
+        string ReceiverEmail { get; }
+        string ReceiverPhone { get; }
+        string ReceiverCity { get; }
+        string ReceiverDistrict { get; }
+        string ReceiverAddressDetail { get; }
+        // Shipink için lazım olacak kargo firması ID'si
+        int CargoCompanyId { get; }
+    }
+}
